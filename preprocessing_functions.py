09f82e4855d96a4ca93a4edd2318a8d2fd95215d
Urszula Baranowska <28218304+umbaranowska@users.noreply.github.com>
2023-01-06 22:07:50 +0100

    started data preprocessing and exploration

---
+++ preprocessing_functions.py
@@ -0,0 +1,49 @@
+import os
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+# %matplotlib inline
+
+def read_image(folder, filename):
+    '''reads patient id, image and label into a dictinary for a single .mat file'''
+    image_dict = {}
+    f = h5py.File(f'{folder}/{filename}','r')
+    image_dict['PID'] = np.array(f.get('cjdata/PID')).transpose()
+    image_dict['image'] = np.array(f.get('cjdata/image'))
+    image_dict['label'] = np.array(f.get('cjdata/label'))
+    image_dict['mask'] = np.array(f.get('cjdata/tumorMask'))
+    return image_dict
+
+def read_images_to_array(folder):
+    '''reads data for all images in a given folder and turns it into numpy arrays'''
+    filenames = os.listdir(folder)
+    images_pid = []
+    images_image = []
+    images_label = []
+    images_mask = []
+    for filename in filenames:
+        image_dict = read_image(folder, filename)
+        images_pid.append(''.join(image_dict['PID'].astype(str).tolist()[0])) # converts pid from array to single string
+        images_image.append(image_dict['image'])
+        images_label.append(image_dict['label'].astype(int)[0][0]) # extracts label from array to single integer
+        images_mask.append(image_dict['mask'])
+    images_pid = np.array(images_pid)
+    images_image = np.array(images_image)
+    images_label = np.array(images_label)
+    images_mask = np.array(images_mask)
+    return images_pid, images_image, images_label, images_mask
+
+def convert_labels_to_strings(images_label):
+    label_dict = {
+        1 : 'meningioma',
+        2 : 'glioma',
+        3 : 'pituitary tumor'
+    }
+    images_label_string = np.array([label_dict[i] for i in images_label])
+    return images_label_string
+
+def plot_image(id, images_pid, images_image, images_label_string, images_mask):
+    plt.imshow(images_image[id], cmap = 'gray')
+    plt.imshow(images_mask[id], cmap = 'Reds', alpha = 0.3)
+    plt.title(f'image {id}, patient ID: {images_pid[id]}, tumor type: {images_label_string[id]}')
+    plt.show()
